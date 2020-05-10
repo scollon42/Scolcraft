7d3449b19f383bff0005d8b2a03dde906adb16a5
Samuel COLLON <samuel.collon@wttj.co>
2020-05-10 13:14:03 +0200

    create window class to wrap glfw3

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,14 @@
-#include <GL/glew.h>
+#include "glew_includes.h"
 #include <GLFW/glfw3.h>
-#include <docopt/docopt.h>
-#include <glm/ext/matrix_transform.hpp>
-#include <glm/geometric.hpp>
 #include <spdlog/spdlog.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <algorithm>
 #include <thread>
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
-
+#include <glm/gtc/type_ptr.hpp>
 #include <Shaders/DefaultProgram.h>
-#include <Utils/PerlinNoise.h>
-
 #include <World/World.h>
 #include <Renderer/ChunkRenderer.h>
 #include <Camera.h>
 #include <Inputs/InputManager.h>
-
-struct DestroyGlfwWin
-{
-  void operator()(GLFWwindow *ptr) const noexcept
-  {
-    glfwDestroyWindow(ptr);
-  }
-};
-
-void framebuffer_size_callback([[maybe_unused]] GLFWwindow *window, int width, int height)
-{
-  glViewport(0, 0, width, height);
-}
+#include <Window.h>
 
 inline void display_ms_per_frame(float time_elapsed) noexcept
 {
@@ -52,25 +29,9 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char **argv)
 {
   spdlog::info("Starting game applicaton");
 
-  glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  auto window = std::make_unique<Window>(800, 600);
 
-  std::unique_ptr<GLFWwindow, DestroyGlfwWin> window(glfwCreateWindow(1600, 1200, "OpenGL", nullptr, nullptr));
-
-  if (window == nullptr) {
-    spdlog::error("Failed to create GLFW window");
-    std::abort();
-  }
-
-  glfwMakeContextCurrent(window.get());
-  glewInit();
-
-  glfwSetFramebufferSizeCallback(window.get(), framebuffer_size_callback);
-  glfwSetInputMode(window.get(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-  auto input_manager = std::make_unique<inputs::InputManager>(*window);
+  auto input_manager = std::make_unique<inputs::InputManager>(window->get_window());
 
   const auto program = shaders::DefaultProgram::build();
 
@@ -91,7 +52,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char **argv)
 
   float last_frame_time{ 0 };
 
-  while (!glfwWindowShouldClose(window.get())) {
+  while (!window->should_close()) {
     const float current_frame_time{ static_cast<float>(glfwGetTime()) };
     const float time_elapsed = current_frame_time - last_frame_time;
     last_frame_time = current_frame_time;
@@ -101,7 +62,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char **argv)
     camera.update(time_elapsed);
 
     if (input_manager->is_pressed(GLFW_KEY_ESCAPE)) {
-      glfwSetWindowShouldClose(window.get(), true);
+      glfwSetWindowShouldClose(window->get_window_ptr(), true);
     }
 
     if (input_manager->is_pressed(GLFW_KEY_F)) {
@@ -120,19 +81,15 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char **argv)
     glUniform3fv(light_position_location, 1, glm::value_ptr(glm::vec3(0.0f, 256.0f, 0.0f)));
     glUniform3fv(view_position_location, 1, glm::value_ptr(camera.get_position()));
 
-    // represent window screen
-    glm::mat4 projection{ 1.0f };
-    projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
-
     program.set_view_uniform(camera.get_view_matrix());
-    program.set_projection_uniform(projection);
+    program.set_projection_uniform(window->get_screen_projection());
 
     glm::mat4 model{ 1.0f };
     program.set_model_uniform(model);
 
     chunk_renderer->render();
 
-    glfwSwapBuffers(window.get());
+    window->swap_buffers();
     glfwPollEvents();
   }
 
