5a2d589ea88170f8bd6c3f7893bd39f5330bc564
Samuel <samuel.collon@protonmail.com>
2020-05-17 15:10:07 +0200

    Add game states to manage game behaviours (#16)

---
+++ src/Game/States/State.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <Window.h>
+#include <Inputs/InputManager.h>
+
+namespace game::states {
+class State
+{
+public:
+  virtual ~State() = default;
+  virtual void init() = 0;
+  virtual void inputs() = 0;
+  virtual void update(float elapsed_time) = 0;
+  virtual void render() = 0;
+
+  void start() noexcept
+  {
+    _is_running = true;
+  }
+
+  void stop() noexcept
+  {
+    _is_running = false;
+  }
+
+  [[nodiscard]] inline bool is_running() const noexcept
+  {
+    return _is_running;
+  }
+
+protected:
+  explicit State(inputs::InputManager &input_manager, Window &window) : _input_manager(input_manager), _window(window)
+  {
+  }
+
+  inputs::InputManager &_input_manager;
+  Window &_window;
+  bool _is_running = false;
+};
+}// namespace game::states
