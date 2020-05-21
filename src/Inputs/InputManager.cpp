fb00044acc841f0109445e140978eb5cbb3e2df3
Samuel <samuel.collon@protonmail.com>
2020-05-21 10:02:51 +0200

    manage player inputs using Event dispatcher (#22)
    * manage player inputs using Event dispatcher
    
    * removing useless commented code

--- src/Inputs/InputManager.cpp
+++ src/Inputs/InputManager.cpp
@@ -3,13 +3,8 @@
 #include <Events/MousePositionEvent.h>
 #include <Events/Dispatcher.h>
 
-void mouse_pos_callback(GLFWwindow *window, double x_position, double y_position)
+void mouse_pos_callback([[maybe_unused]] GLFWwindow *window, double x_position, double y_position)
 {
-  const auto input_manager = static_cast<inputs::InputManager *>(glfwGetWindowUserPointer(window));
-
-  input_manager->update_mouse_position(glm::vec2{
-    static_cast<float>(x_position), static_cast<float>(y_position) });
-
   events::MousePositionEvent event{ static_cast<float>(x_position), static_cast<float>(y_position) };
   events::Dispatcher::get().dispatch<events::MousePositionEvent>(event);
 }
@@ -23,21 +18,8 @@ void keyboard_callback([[maybe_unused]] GLFWwindow *window, int key, [[maybe_unu
 inputs::InputManager::InputManager(GLFWwindow &window)
   : _window(window)
 {
-  glfwSetWindowUserPointer(&_window, this);
   glfwSetCursorPosCallback(&_window, mouse_pos_callback);
   glfwSetKeyCallback(&_window, keyboard_callback);
-}
-
-void inputs::InputManager::subscribe_to_mouse_event(const std::function<void(glm::vec2)> &callback) noexcept
-{
-  _mouse_event_subscribers.push_back(callback);
-}
-
-void inputs::InputManager::update_mouse_position(const glm::vec2 &mouse_position) const noexcept
-{
-  for (const auto &cb : _mouse_event_subscribers) {
-    cb(mouse_position);
-  }
 }
 
 bool inputs::InputManager::is_pressed(int key_code) const
--- src/Inputs/InputManager.h
+++ src/Inputs/InputManager.h
@@ -12,15 +12,9 @@ class InputManager final
 public:
   explicit InputManager(GLFWwindow &window);
 
-  void subscribe_to_mouse_event(const std::function<void(glm::vec2)> &callback) noexcept;
-  //TODO add unsubscribe
-
-  void update_mouse_position(const glm::vec2 &mouse_position) const noexcept;
-
   [[nodiscard]] bool is_pressed(int key_code) const;
 
 private:
-  GLFWwindow &                                _window;
-  std::vector<std::function<void(glm::vec2)>> _mouse_event_subscribers;
+  GLFWwindow &_window;
 };
 }// namespace inputs
